d66b0a1ca9454367d6bfae4f8bd59b922b2e7b95
Andrew Williams <1williamsaj@gmail.com>
2019-08-19 01:46:26 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -2,4 +2,5 @@
 
 def greeting(name) #Our greeting method will take one argument: the user's name
   puts "Hello #{name.strip}. It's nice to meet you." #We interpolate the variable 'name' into the string we want to output to the user
+  #To remove the additional whitespace around the person's name, just add '.strip'. 
 end
