0d7fcf53098d117db02b6cf4fda6b871aa3f2f0d
Andrew Williams <1williamsaj@gmail.com>
2019-08-19 01:43:09 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,5 +1,5 @@
 # code the #greeting method here!
 
 def greeting(name) #Our greeting method will take one argument: the user's name
-  puts "Hello #{name}. It's nice to meet you." #We interpolate the variable 'name' into the string we want to output to the user
+  puts "Hello #{name}.strip. It's nice to meet you." #We interpolate the variable 'name' into the string we want to output to the user
 end
