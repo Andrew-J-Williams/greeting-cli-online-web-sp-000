0f35b62c6ae697f819e4f3dad54e33e5a2b81ccb
Andrew Williams <1williamsaj@gmail.com>
2019-08-19 01:33:09 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,5 +1,8 @@
 # code the #greeting method here!
 
+name = gets.strip
+
 def greeting(name) #Our greeting method will take one argument: the user's name
   puts "Hello #{name}. It's nice to meet you."
 end
+
